82fdbf67731a566fee0811309bd7d53614149eed
XxCesar73xX <cesarimenez.f123@gmail.com>
2025-11-04 10:31:13 +0100

    he editado el readme - Cesar

--- gestUsuarios.bash
+++ gestUsuarios.bash
@@ -2,10 +2,10 @@
 source funciones.bash
 
 function menuGestUser() {
-    echo "comprobando paquetes necesarios..."
+    echo "comprobando/actualizando paquetes necesarios..."
     touch /home/"$(whoami)"/.gestorLinux
     if ! [[ "$(cat /home/"$(whoami)"/.gestorLinux | cut -d: -f1)" == "OK" ]]; then
-        if ! sudo apt update &> /dev/null; then
+        if ! sudo apt update; then
             echo "❌ Error: fallo al actualizar los paquetes..."
         else
             echo "OK:" > /home/"$(whoami)"/.gestorLinux
