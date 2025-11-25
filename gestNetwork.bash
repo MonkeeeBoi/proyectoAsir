99d019a3e005997da7b5ddd8bff030d60a7edd71
MonkeeBoi <alvaro290602@gmail.com>
2025-11-25 10:02:49 +0100

    feat: enhance network configuration menu with interface validation and improved YAML generation

--- gestNetwork.bash
+++ gestNetwork.bash
@@ -10,9 +10,7 @@ function menuGestNetwork() {
     echo "|   1. Mostrar configuración de red   |"
     echo "|   2. Probar conectividad            |"
     echo "|   3. Reiniciar red                  |"
-    echo "|   4. Ver IP                         |"
-    echo "|   5. Cambiar IP                     |"
-    echo "|   6. Cambiar DNS                    |"
+    echo "|   4. Cambiar configuracion de red   |"
     echo "|                                     |"
     echo "|   0. Volver                         |"
     echo "+-------------------------------------+"
@@ -41,45 +39,81 @@ function menuGestNetwork() {
             clear
             ;;
         4)
-            ip_actual=$(ip -4 addr show | grep -oP '(?<=inet\s)\d+(\.\d+){3}' | head -n1)
-            echo "IP actual: $ip_actual"
-            read -n1 -srp "Presione una tecla para continuar..."
-            clear
-            ;;
-        5)
-            # Pedir datos de configuración
-            read -p "Nombre de la interfaz de red (ej: eth0): " interfaz
-            read -p "Dirección IP con máscara (ej: 192.168.1.100/24): " ip
-            read -p "Gateway (ej: 192.168.1.1): " gateway
-            read -p "Servidores DNS separados por comas (ej: 8.8.8.8,8.8.4.4): " dns
-
+            read -rp "Nombre de la interfaz de red (ej: eth0): " interfaz
+            while true; do
+                if ! ip a | grep -E "$interfaz:"; then
+                    echo "La Interfaz no existe en el dispositivo..."
+                    continue
+                fi
+                break
+            done
             NETPLAN_DIR="/etc/netplan/$interfaz.yaml"
-            # Crear configuración en el fichero YAML
-            sudo tee $NETPLAN_DIR > /dev/null <<EOF
+            while true; do 
+                read -rp "Quires que sea dhcp [Y/n]: " yesOrNo
+                if comprobarYesOrNo "$yesOrNo"; then
+                    continue
+                fi
+                if YesOrNo "$yesOrNo"; then
+                    sudo tee "$NETPLAN_DIR" > /dev/null <<EOF
 network:
   version: 2
-  renderer: networkd
+  renderer: NetworkManager
+  ethernets:
+    $interfaz:
+        dhcp4: yes
+
+EOF
+                else
+                    while true; do
+                        read -rp "Dirección IP con máscara (ej: 192.168.1.100/24): " ip
+                        if ! echo "$ip" | grep -E "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\/[1-9]{1,2}"; then
+                            echo "ip no valida..."
+                            continue
+                        fi
+                        break
+                    done
+                    
+                    while true; do
+                        read -rp "Gateway (ej: 192.168.1.1): " gateway
+                        if ! echo "$gateway" | grep -E "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}"; then
+                            echo "ip no valida..."
+                            continue
+                        fi
+                        break
+                    done
+                    
+                    while true; do
+                        read -rp "Servidores DNS separados por comas (ej: 8.8.8.8,8.8.4.4): " dns
+                        if ! echo "$gateway" | grep -E "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}"; then
+                            echo "ip no valida..."
+                            continue
+                        fi
+                        break
+                    done
+                    sudo tee "$NETPLAN_DIR" > /dev/null <<EOF
+network:
+  version: 2
+  renderer: NetworkManager
   ethernets:
     $interfaz:
       dhcp4: no
       addresses: [$ip]
-      gateway4: $gateway
+      routes:
+        - to: default
+          via: $gateway
       nameservers:
         addresses: [${dns// /}]
 EOF
-
-            echo "Fichero Netplan configurado en $NETPLAN_DIR"
-            echo "Aplicando configuración..."
-            sudo netplan apply
-            read -n1 -srp "Presione una tecla para continuar..."
-            clear
-            ;;
-        6)
-            read -rp "Introduce la nueva IP de DNS (ej: 8.8.8.8): " nuevo_dns
-            echo "nameserver $nuevo_dns" | sudo tee /etc/resolv.conf > /dev/null
-            echo "DNS cambiado a $nuevo_dns"
-            read -n1 -srp "Presione una tecla para continuar..."
-            clear
+                    sudo chmod 600 "$NETPLAN_DIR"
+                    echo "Fichero Netplan configurado en $NETPLAN_DIR"
+                    echo "Aplicando configuración..."
+                    sudo netplan apply
+                    read -n1 -srp "Presione una tecla para continuar..."
+                    clear
+                fi
+                break
+            done
+            
             ;;
         0)
             break
